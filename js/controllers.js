7a9711ce81ef4df4568c1f384f8e3d8c8e928fb1
bon22801 <bon22801@gmail.com>
2015-11-01 18:46:31 -0500

    sort player by score and fix indention

--- js/controllers.js
+++ js/controllers.js
@@ -7,14 +7,15 @@
 			return {
 				getPlayerData: function(callback) {
 					var key = '84c3d590-07e1-4ed6-bf20-94931e215bd1';
-	  			$http.get('https://na.api.pvp.net/api/lol/na/v2.5/league/challenger?type=RANKED_SOLO_5x5&api_key='+key).success(callback);
+					$http.get('https://na.api.pvp.net/api/lol/na/v2.5/league/challenger?type=RANKED_SOLO_5x5&api_key='+key).success(callback);
 				}
 			}
 		})
 	
 		.controller("playerCtrl", function ($scope, playerService){
 			playerService.getPlayerData(function(data) {
-				$scope.players = data.entries;														
+				$scope.players = data.entries;
+				$scope.orderProp = '-leaguePoints';
 			});
 		})	
 })();
