f6b15b485b2545ab86aa82c2b4da9a51635d5801
bon22801 <bon22801@gmail.com>
2015-11-01 18:09:05 -0500

    put json loading in service

--- js/controllers.js
+++ js/controllers.js
@@ -3,20 +3,18 @@
 
 	angular
 		.module('leaderboardApp', [])
-	  .controller('leagueController', leagueController);
-
-	 function leagueController($scope, $http) {
-	  var key = '84c3d590-07e1-4ed6-bf20-94931e215bd1';
-	    
-	  // Get json data from league-v2.5 API. See https://developer.riotgames.com/api/methods#!/985
-	  $http.get('https://na.api.pvp.net/api/lol/na/v2.5/league/challenger?type=RANKED_SOLO_5x5&api_key='+key).success(function(data) {
-	    $scope.players = data.entries;
-	  });
-	    
-	  // Sort players from the heightest leaguePoints to the lowest.
-	    $scope.orderProp = '-leaguePoints';
-	 };
-
+		.factory("playerService", function ($http){
+			return {
+				getPlayerData: function(callback) {
+					var key = '84c3d590-07e1-4ed6-bf20-94931e215bd1';
+	  			$http.get('https://na.api.pvp.net/api/lol/na/v2.5/league/challenger?type=RANKED_SOLO_5x5&api_key='+key).success(callback);
+				}
+			}
+		})
+	
+		.controller("playerCtrl", function ($scope, playerService){
+			playerService.getPlayerData(function(data) {
+				$scope.players = data.entries;														
+			});
+		})	
 })();
-
-
