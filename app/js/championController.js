8eea615d8eda1e518a7f5c1cf16b7b8b39c62b4c
bon22801 <bon22801@gmail.com>
2016-12-12 19:19:57 -0500

    implemented part of champion info

--- app/js/championController.js
+++ app/js/championController.js
@@ -2,21 +2,21 @@
     'use strict';
 
     angular.module('leaderboardApp').controller("ChampionCtrl", ['championService', function (championService) {
-
+        this.champSelected = false;
+        this.selectedChampion = [];
+        
 		    championService.getChampionImages((data) => {
 		        this.champions = data;
         });
         
         this.selectChampion = (id) => {
             championService.selectedChampionId = id;
-            console.log(championService.selectedChampionId);
             
             championService.getChampionStats((data) => {
-                console.log(data);
+                this.selectedChampion = data;
             });
-            
+            this.champSelected = true;
         }
-        
 		}])
 })();
 
