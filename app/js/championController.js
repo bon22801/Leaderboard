a1ba2ecc4326cdcce5c3e60232de838dd548db9a
bon22801 <bon22801@gmail.com>
2017-03-02 23:55:53 -0500

    built championID -> key mapping

--- app/js/championController.js
+++ app/js/championController.js
@@ -7,7 +7,19 @@
         
 		    championService.getChampionImages((data) => {
 		        this.champions = data;
+            championService.championMap = this.buildChampionMapping(data);
         });
+        
+        this.buildChampionMapping = (champions) => {
+            let map = {};
+            
+            for (var prop in champions.data) {
+                let id = champions.data[prop].id;
+                let key = champions.data[prop].key;
+                map[id] = key;
+            }
+            return map;
+        }
         
         this.selectChampion = (id) => {
             championService.selectedChampionId = id;
--- app/js/playerController.js
+++ app/js/playerController.js
@@ -2,16 +2,16 @@
     'use strict';
 
     angular
-        .module('leaderboardApp').controller("PlayerCtrl",  ['playerService', function (playerService) {
+        .module('leaderboardApp').controller("PlayerCtrl",  ['playerService', 'championService', function (playerService, championService) {
         playerService.getPlayers((playerList) => {
-            console.log(playerList);
             this.players = playerList.entries;
         });
 
         this.selectPlayer = (id) => {
             playerService.selectedPlayerId = id;
             playerService.getChampionStats((data) => {
-                console.log(data);   
+                console.log(data);
+                console.log(championService.championMap[1]);
             });
         }
 
